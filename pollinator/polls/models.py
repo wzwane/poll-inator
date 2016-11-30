0f518667b951193aeee7ae0fa386fa7700591316
HyunUk Jeong <hjeong326@gmail.com>
2016-11-30 02:03:33 -0600

    made Questions and Choices unique

--- pollinator/polls/models.py
+++ pollinator/polls/models.py
@@ -3,11 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Question(models.Model):
-	question_text = models.CharField(max_length=200)
+	question_text = models.CharField(max_length=200, unique=True)
 	pub_date = models.DateTimeField('date published')
+
+	def __str__(self):
+		return 'Question: %s' % (self.question_text)
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
-	choice_text = models.CharField(max_length=200)
+	choice_text = models.CharField(max_length=200, unique=True)
 	votes = models.IntegerField(default=0)
+
+	def __str__(self):
+		return 'Choice: %s' % (self.choice_text)
 

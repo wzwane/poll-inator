d98ffce8982ccab04c52f3790fc6b70139d0115a
HyunUk Jeong <hjeong326@gmail.com>
2016-12-01 03:40:50 -0600

    Voting works now. Can't see results yet.

--- pollinator/polls/views.py
+++ pollinator/polls/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from .models import Question, Choice
 from .forms import QuestionForm
+from django.urls import reverse
 
 def index(request):
 	question_list = Question.objects.all()
@@ -21,26 +22,39 @@ def index(request):
 	return render(request, 'polls/index.html', context)
 """
 
+"""Original detail() method"""
+"""
 def detail(request, question_id):
 	try:
 		question = Question.objects.get(pk=question_id)
 	except Question.DoesNotExist:
 		raise Http404("Question does not exist")
 	return render(request, 'polls/detail.html', {'question': question})
-
-"""Just making use of get_object_or_404() method"""
 """
+
+"""Making use of get_object_or_404() method"""
 def detail(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
 	return render(request, 'polls/detail.html', {'question': question})
-"""
+
 
 def results(request, question_id):
 	response = "You're looking at the results of question %s."
 	return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message': "You didn't select a choice.",
+			})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('results', args=(question.id,)))
 
 def mainPage(request):
 	question_list = Question.objects.all()

d42dfb4e665ee2f9670f3968e364224c76429893
el0ise <eterry@macalester.edu>
2016-12-01 21:35:39 -0600

    Merge branch 'master' of /Users/eloise/PycharmProjects/poll-inator with conflicts.

--- pollinator/polls/views.py
+++ pollinator/polls/views.py
@@ -70,3 +70,27 @@ def get_question(request):
 		else:
 			form = QuestionForm()
 		return render(request, 'main.html', {'form': form})
+
+def create_post(request):
+    if request.method == 'POST':
+        post_text = request.POST.get('the_post')
+        response_data = {}
+
+        post = Post(text=post_text, author=request.user)
+        post.save()
+
+        response_data['result'] = 'Create post successful!'
+        response_data['postpk'] = post.pk
+        response_data['text'] = post.text
+        response_data['created'] = post.created.strftime('%B %d, %Y %I:%M %p')
+        response_data['author'] = post.author.username
+
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type="application/json"
+        )
+    else:
+        return HttpResponse(
+            json.dumps({"nothing to see": "this isn't happening"}),
+            content_type="application/json"
+        )

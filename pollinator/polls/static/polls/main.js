5f990979cd2616886de6bc93da1cd6906e5967bc
HyunUk Jeong <hjeong326@gmail.com>
2016-12-03 23:19:55 -0600

    moved javascript code from index.html to main.js

--- pollinator/polls/static/polls/main.js
+++ pollinator/polls/static/polls/main.js
@@ -1,34 +1,68 @@
 // Tutorial from: https://realpython.com/blog/python/django-and-ajax-form-submissions/
-$('#post-form').on('submit', function(event){
-    event.preventDefault();
-    console.log('form submitted!')
-    create_post();
+$(document).ready(function(){
+    $('#post-form').on('submit', function(event){
+        event.preventDefault();
+        console.log('form submitted!')
+        create_post();
+    });
 
-})
+    $('#question-form').on('submit', function(event){
+        event.preventDefault();
+        console.log("Question submitted!")
+        create_question();
+    });
 
-function create_post(){
-    console.log("You've successfully created a post!")
-    $.ajax({
-        url : "create_post/",
-        type : "POST",
-        data : { a_post : $('#post-text').val() },
+    function create_post(){
+        console.log("You've successfully created a post!")
+        $.ajax({
+            url : "create_post/",
+            type : "POST",
+            data : { a_post : $('#post-text').val() },
 
-        success : function(json) {
-            $('#post-text').val('');
-            console.log(json);
-            console.log("SUCCESS!");
+            success : function(json) {
+                $('#post-text').val('');
+                console.log(json);
+                console.log("SUCCESS!");
 
-        },
+            },
 
-        error : function(xhr,errmsg,err) {
-            $('#results').html("<div class='alert-box alert radius' data-alert>Oops! Error: "+errmsg+
-                " <a href='#' class='close'>&times;</a></div>");
-            console.log(xhr.status + ": " + xhr.responseText);
-        }
+            error : function(xhr,errmsg,err) {
+                $('#results').html("<div class='alert-box alert radius' data-alert>Oops! Error: "+errmsg+
+                    " <a href='#' class='close'>&times;</a></div>");
+                console.log(xhr.status + ": " + xhr.responseText);
+            }
+        });
+    }
+
+    var questionsList = [];
+    var questionId = 0;
+    var newQuestionTitle = document.getElementById("questionTitle");
+    var newQuestionDescription = document.getElementById("questionDescription");
 
 
+    function insertQ () {
+        displayQ();
+        clearQ();
+    }
 
-    })
+    function displayQ(){
+        questionId += 1;
+        console.log(questionId)
+        questionsList.push({id: questionId, title: newQuestionTitle.value, des:newQuestionDescription.value })
+        $(".questions").append($('<div class="question shadowedBox">')
+        .append($('<div class="questionInfo">')
+            .append($('<h3 class="questionTitle">')
+            .append($("<p>").append(newQuestionTitle.value)))
+            .append($('<p class="questionContent">').append($("<p>").append(newQuestionDescription.value))))
+            .append($('<div class="options"><button class="btn btn-primary upvote">^</button><button class="btn btn-primary downvote">V</button><button class="btn btn-primary comment">Comment</button>')
+
+            ));
+        console.log(questionsList)
+    }
 
 
-}
+    function clearQ(){
+        newQuestionTitle.value = "";
+        newQuestionDescription.value = "";
+    }
+});
